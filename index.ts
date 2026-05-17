87fc065d2b89a647062f5c799c1e073750300725
Andrew Wittrock <andy@wittrock.us>
2026-05-17 14:15:43 -0500

    Add FieldRegistry to only allow some fields to be updated via the API

--- index.ts
+++ index.ts
@@ -1,6 +1,15 @@
+import { FieldRegistry } from "@core/express/util";
 import { init } from "../analytics/migrations/00-init";
 
 export { AnalyticsEndpoints as apiConfig } from "./endpoints";
 
 export const migrations = [init];
 export const setupMigrations = [init];
+
+FieldRegistry.register(
+    "analyticsEvents",
+    [
+        "url", "host", "referrer", "userAgent", "browserName", "browserVersion", "os",
+        "ipAddress", "geoCountry", "geoRegion", "geoCity", "screenWidth", "screenHeight", "timestamp"
+    ]
+);
